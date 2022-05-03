8869b89a5e5e32ece89d2e6c96e88b92a73ff566
Alexandre Drevet <alexandre.drevet1@gmail.com>
2022-05-03 15:25:56 +0200

    formatage_1.py

--- SCRIPT/formatage_1.py
+++ SCRIPT/formatage_1.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
-list_ronde = ()
-DATE_RDE = datetime.now()
-#print (DATE_RDE)
-TPS_RDE = str('000')
 
-list_ronde = '(' +  TPS_RDE + ", " + "'" + str(DATE_RDE) + "'" + ')' + ';'
-print (list_ronde)
+def formatage_1():
+    output = ""
+    DATE_RDE = datetime.now()
+    #print (DATE_RDE)
+    TPS_RDE = str('000')
+    output = '(' +  TPS_RDE + ", " + "'" + str(DATE_RDE) + "'" + ')' + ';'
+    return output
+print (formatage_1())
